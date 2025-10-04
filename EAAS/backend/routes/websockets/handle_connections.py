b7e184792be2c8b606c8c04ed68009f76fb81641
amstrdm <a.caserta@proton.me>
2025-10-04 19:43:17 +0200

    set room limit to 2

--- EAAS/backend/routes/websockets/handle_connections.py
+++ EAAS/backend/routes/websockets/handle_connections.py
@@ -1,7 +1,7 @@
 import asyncio
 from typing import Dict, Set
 
-from fastapi import WebSocket, WebSocketDisconnect
+from fastapi import WebSocket, status
 
 room_connections: Dict[str, Set[WebSocket]] = {}
 
@@ -10,12 +10,21 @@ class ConnectionManager:
     """Manages websocket connections"""
 
     @staticmethod
-    async def connect(websocket: WebSocket, room_phrase: str):
+    async def connect(websocket: WebSocket, room_phrase: str) -> bool:
         """Add connection to room"""
+        current_connections = room_connections.get(room_phrase, set())
+        if len(current_connections) >= 2:
+            await websocket.accept()
+            await websocket.close(
+                code=status.WS_1008_POLICY_VIOLATION, reason="Room is full"
+            )
+            return False
+
         await websocket.accept()
         if room_phrase not in room_connections:
             room_connections[room_phrase] = set()
         room_connections[room_phrase].add(websocket)
+        return True
 
     @staticmethod
     def disconnect(websocket: WebSocket, room_phrase: str):
