ab29326e84df9a0369d578a96c3ffb36f2cee9c3
amstrdm <a.caserta@proton.me>
2025-10-05 12:01:02 +0200

    new links

--- EAAS/frontend/eaas-frontend/src/api/config.ts
+++ EAAS/frontend/eaas-frontend/src/api/config.ts
@@ -1,4 +1,2 @@
-// export const API_BASE_URL = "https://eaas.epistemesoftware.com/api";
-// export const WS_BASE_URL = "ws://eaas.epistemesoftware.com/api/ws";
-export const API_BASE_URL = "http://localhost:8000/api";
-export const WS_BASE_URL = "ws://localhost:8000/api/ws";
+export const API_BASE_URL = "https://eaas.epistemesoftware.com/api";
+export const WS_BASE_URL = "ws://eaas.epistemesoftware.com/api/ws";
